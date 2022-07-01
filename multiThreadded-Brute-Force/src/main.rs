25fad2be331e1104c152800de0816512bd4c6b05
ZGibbs19 <zacgib19@gmail.com>
2022-07-01 17:23:38 -0500

    Fixed user Input syntax

--- multiThreadded-Brute-Force/src/main.rs
+++ multiThreadded-Brute-Force/src/main.rs
@@ -3,19 +3,19 @@ use std::io;
 
 fn main() {
     let mut maxLength: i16 = 0;
-    let choice: char = '';
+    let mut input_text = String::new();
+    let choice: char = ' ';
     let hasGuessed: bool = false;
     let numGuesses: u32 = 0;
-    printf("Hello! Welcome to this brute force program!"
-           "\nHere, you will be prompted for a password, and the computer will "
-           "attempt to brute force guess the password. "
-           "\nPlease enter the maximum length of password you want to guess: ");
+    println!("\n\n\nHello! Welcome to this brute force program!\
+             \nHere, you will be prompted for a password, and the computer will\
+             attempt to brute force guess the password.\
+             \nPlease enter the maximum length of password you want to guess: ");
 
     io::stdin() // Incoporate user validation somehow
-        .read_line(&mut maxLength)
-        .except("Failed to read line");
+        .read_line(&mut input_text)
+        .expect("Failed to read line");
 
-    
 
 
 }
